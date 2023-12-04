3ce70463a4ce76e08d71d1687e61ef9786b80519
yekang <1847016090@qq.com>
2023-12-04 18:39:10 +0800

    fix:通用编程概念

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,30 @@ use guest_game_02::{classes, homework};
 use std::{fs, io::ErrorKind};
 
 fn main() {
-    print_hello_world();
-    homework::transfer_temperature_to_hua(1.0);
-    homework::transfer_temperature_to_wen(33.8);
-    classes::guest_game();
-    println!("斐波那契数列。");
-    let input: i8 = 6;
-    let value = homework::fib(input);
-    println!("fib({})={}", input, value);
+    let mut arr = Vec::new();
+    arr.push(1);
+    // print_hello_world();
+    // homework::transfer_temperature_to_hua(1.0);
+    // homework::transfer_temperature_to_wen(33.8);
+    // classes::guest_game();
+    // println!("斐波那契数列。");
+    // let input: i8 = 6;
+    // let value = homework::fib(input);
+    // println!("fib({})={}", input, value);
 
-    println!("读取歌词循环打出歌词");
-    let mut content: String = match homework::deal_song() {
-        Ok(val) => val,
-        Err(err) => {
-            return match err.kind() {
-                ErrorKind::NotFound => {
-                    fs::File::create("./static/song.txt").unwrap();
-                }
-                _ => {
-                    println!("err={}", err);
-                }
-            };
-        }
-    };
-    println!("歌词={:?}", &content.split("\n\n"))
+    // println!("读取歌词循环打出歌词");
+    // let mut content: String = match homework::deal_song() {
+    //     Ok(val) => val,
+    //     Err(err) => {
+    //         return match err.kind() {
+    //             ErrorKind::NotFound => {
+    //                 fs::File::create("./static/song.txt").unwrap();
+    //             }
+    //             _ => {
+    //                 println!("err={}", err);
+    //             }
+    //         };
+    //     }
+    // };
+    // println!("歌词={:?}", &content.split("\n\n"))
 }
