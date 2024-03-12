1ed66d8c2920e0787bade15c96e46bde2af2d5d9
yekang <1847016090@qq.com>
2024-03-12 09:06:28 +0800

    fix: 高级特性

--- src/main.rs
+++ src/main.rs
@@ -110,19 +110,59 @@
 use core::num;
 
 fn main() {
-    struct Message {
-        id: i32,
+    use std::slice;
+
+    fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+        let len = slice.len();
+        let ptr = slice.as_mut_ptr();
+
+        assert!(mid <= len);
+
+        unsafe {
+            (
+                slice::from_raw_parts_mut(ptr, mid),
+                slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
+            )
+        }
     }
 
-    let msg = Message { id: 3 };
-    match msg {
-        Message { id: sub_id @ 1..=7 } => {
-            println!("{}", sub_id)
-        }
-        Message { id } => {
-            println!("{}", id)
-        }
-    }
+    // let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    // let r = &mut v[..];
+
+    // let (a, b) = r.split_at_mut(3);
+
+    // assert_eq!(a, &mut [1, 2, 3]);
+    // assert_eq!(b, &mut [4, 5, 6]);
+    // fn dangerous() {}
+
+    // unsafe {
+    //     dangerous();
+    // }
+
+    // let mut num = 5;
+
+    // let r1 = &num as *const i32;
+    // let r2 = &mut num as *mut i32;
+
+    // unsafe {
+    //     println!("r1={}", *r1);
+    //     println!("r2={}", *r2+1);
+    // }
+
+    // struct Message {
+    //     id: i32,
+    // }
+
+    // let msg = Message { id: 3 };
+    // match msg {
+    //     Message { id: sub_id @ 1..=7 } => {
+    //         println!("{}", sub_id)
+    //     }
+    //     Message { id } => {
+    //         println!("{}", id)
+    //     }
+    // }
 
     // let x: Option<i32> = Some(5);
 
